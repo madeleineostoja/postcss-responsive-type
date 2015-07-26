1b410126df68fe804db5e85d3c02ee471053a661
Sean King <sean@seanking.org>
2015-07-26 13:35:20 +1000

    init

---
+++ index.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-responsive-type', function () {
+
+  // Default parameters
+  var params = {
+    minSize: '12px',
+    maxSize: '21px',
+    minWidth: '420px',
+    maxWidth: '1280px'
+  };
+
+  // Map expanded declarations to params
+  var paramDecls = {
+    minSize: 'min-font-size',
+    maxSize: 'max-font-size',
+    minWidth: 'lower-font-range',
+    maxWidth: 'upper-font-range'
+  };
+
+  /**
+   * Fetch plugin parameters from css rules
+   * @param  {object} rule CSS rule to parse
+   */
+  var fetchParams = function(rule){
+
+    // Fetch params from shorthand font-size
+    rule.eachDecl('font-size', function(decl){
+
+      if (decl.value.indexOf('responsive') > -1) {
+        var vals = decl.value.match(/([0-9]+\w*)/g);
+
+        if (vals){
+          params.minSize = vals[0];
+          params.maxSize = vals[1];
+        }
+      }
+
+    });
+
+    // Fetch params from shorthand font-range
+    rule.eachDecl('font-range', function(decl){
+      var vals = decl.value.split(/\s+/);
+      params.minWidth = vals[0];
+      params.maxWidth = vals[1];
+      decl.removeSelf();
+    });
+
+    // Fetch parameters from expanded properties
+    Object.keys(paramDecls).forEach(function(param){
+      rule.eachDecl(paramDecls[param], function(decl){
+        params[param] = decl.value.trim();
+        decl.removeSelf();
+      });
+    });
+
+  };
+
+  /**
+   * Build new responsive type rules
+   * @param  {object} rule     old CSS rule
+   * @param  {object} newRules object to store new CSS rules
+   * @return {object}          object of new CSS rules
+   */
+  var buildRules = function(rule, newRules) {
+
+    // Build the responsive decleration
+    var sizeDiff = parseFloat(params.maxSize) - parseFloat(params.minSize),
+        widthDiff = parseFloat(params.maxWidth) - parseFloat(params.minWidth);
+
+    newRules.responsive = 'calc(' + params.minSize + ' + ' + sizeDiff.toString() + ' * ((100vw - ' + params.minWidth + ') / ' + widthDiff.toString() + '))';
+
+    // Build the media queries
+    newRules.minMedia = postcss.atRule({
+      name: 'media',
+      params: 'screen and (max-width: ' + params.minWidth + ')',
+      before: '\n'
+    });
+
+    newRules.maxMedia = postcss.atRule({
+      name: 'media',
+      params: 'screen and (min-width: ' + params.maxWidth + ')',
+      before: '\n'
+    });
+
+    // Add the required content to new media queries
+    newRules.minMedia.append({
+        selector: rule.selector
+    }).eachRule(function(selector){
+      selector.append({
+        prop: 'font-size',
+        value: params.minSize
+      });
+    });
+
+    newRules.maxMedia.append({
+      selector: rule.selector
+    }).eachRule(function(selector){
+      selector.append({
+        prop: 'font-size',
+        value: params.maxSize
+      });
+    });
+
+    return newRules;
+  };
+
+  // Do it!
+  return function (css) {
+    css.eachRule(function(rule){
+
+      var thisRule,
+          newRules = {};
+
+      rule.eachDecl('font-size', function(decl){
+
+        // If rule doesn't contain responsve keyword, exit
+        if (!decl.value.indexOf('responsive') > -1) {
+          return;
+        }
+
+        // Otherwise store the rul in a var
+        thisRule = decl.parent;
+
+      });
+
+      fetchParams(rule);
+
+      buildRules(rule, newRules);
+
+      // Insert the base responsive decleration
+      rule.eachDecl('font-size', function(decl){
+        if (decl.value.indexOf('responsive') > -1) {
+          decl.replaceWith({prop: decl.prop, value: newRules.responsive });
+        }
+      });
+
+      // Insert the media queries
+      rule.parent.insertAfter(thisRule, newRules.minMedia);
+      rule.parent.insertAfter(newRules.minMedia, newRules.maxMedia);
+
+    });
+  };
+
+});
---
+++ test/test.js
@@ -0,0 +1,51 @@
+/*eslint no-unused-expressions: 0, block-scoped-var: 0, no-undef: 0*/
+'use strict';
+
+var postcss = require('postcss'),
+    expect = require('chai').expect,
+    fs = require('fs'),
+    path = require('path'),
+    plugin = require('../');
+
+var test = function (fixture, opts, done) {
+  var input = fixture + '.css',
+      expected = fixture + '.expected.css';
+
+  input = fs.readFileSync(path.join(__dirname, 'fixtures', input), 'utf8');
+  expected = fs.readFileSync(path.join(__dirname, 'fixtures', expected), 'utf8');
+
+  postcss([ plugin(opts) ])
+    .process(input)
+    .then(function (result) {
+      expect(result.css).to.eql(expected);
+      expect(result.warnings()).to.be.empty;
+    done();
+  }).catch(function (error) {
+    done(error);
+  });
+
+};
+
+describe('postcss-responsive-type', function () {
+
+  it('builds responsive type with defaults', function (done) {
+   test('default', {}, done);
+  });
+
+  it('applies custom parameters', function (done) {
+   test('custom', {}, done);
+  });
+
+  it('works with shorthand properties', function (done) {
+   test('shorthand', {}, done);
+  });
+
+  it('doesn\'t kill fallbacks/duplicate properties', function (done) {
+   test('fallback', {}, done);
+  });
+
+  it('sanitizes inputs', function (done) {
+   test('formatting', {}, done);
+  });
+
+});
